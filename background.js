24603c58f5e3b7b24ff82ac64fb0599ac79c3d36
Ashraf Mohammed <ashrafmd0506@gmail.com>
2023-10-14 16:22:54 +0530

    add variable filters

--- background.js
+++ background.js
@@ -1,15 +1,9 @@
 links = {
-    "sales revenue dashboard":
-        "https://app.powerbi.com/groups/me/reports/07353a90-7b2a-44fe-92d3-d6b8565fa43e/ReportSection8da7d6bdc07307d92cd8?experience=power-bi",
-    "production dashboard":
-        "https://app.powerbi.com/groups/me/reports/b5d3ec85-a88d-4241-8dca-61c3852e3f66/ReportSection0b965e5f0ff4c1d12917?experience=power-bi",
-    "cost analysis dashboard":
-        "https://app.powerbi.com/groups/me/reports/b5d3ec85-a88d-4241-8dca-61c3852e3f66/ReportSection76e661661e1efa82616c?experience=power-bi",
-    "raw material dashboard":
-        "https://app.powerbi.com/groups/me/reports/b5d3ec85-a88d-4241-8dca-61c3852e3f66/ReportSectionfbdc3e3fdaae956b9024?experience=power-bi",
-    // https://app.powerbi.com/groups/me/reports/b4a53daa-a519-44ca-8b10-bcb1b8e7cfcf/ReportSectionfbdc3e3fdaae956b9024?filter=RM_x0020_Price/Plant eq 'Bhurwal'
+    "sales revenue dashboard": "https://app.powerbi.com/groups/me/reports/07353a90-7b2a-44fe-92d3-d6b8565fa43e/ReportSection8da7d6bdc07307d92cd8?experience=power-bi",
+    "production dashboard": "https://app.powerbi.com/groups/me/reports/b5d3ec85-a88d-4241-8dca-61c3852e3f66/ReportSection0b965e5f0ff4c1d12917?experience=power-bi",
+    "cost analysis dashboard": "https://app.powerbi.com/groups/me/reports/b5d3ec85-a88d-4241-8dca-61c3852e3f66/ReportSection76e661661e1efa82616c?experience=power-bi",
+    "raw material dashboard": "https://app.powerbi.com/groups/me/reports/b5d3ec85-a88d-4241-8dca-61c3852e3f66/ReportSectionfbdc3e3fdaae956b9024?experience=power-bi", // https://app.powerbi.com/groups/me/reports/b4a53daa-a519-44ca-8b10-bcb1b8e7cfcf/ReportSectionfbdc3e3fdaae956b9024?filter=RM_x0020_Price/Plant eq 'Bhurwal'
 };
-
 const filters = ["Anara", "BBSR", "Bhurwal", "BLSPR CP", "BLSPR LL", "Gaya", "Hubli", "Mirza", "Pathri", "Sholaka", "TMQ", "Udvada", "Wadiyaram"];
 
 function getLinks(name) {
@@ -30,8 +24,14 @@ function giveFilters(filterValues) {
     let allFiltersIndex = []
     for (filterValue of filterValues) {
         for (filter of filters) {
-            if (filter.includes(filterValue.slice(0, 1).toUpperCase() + filterValue.slice(1).toLowerCase())) {
-                allFiltersIndex.push(filters.indexOf(filter))
+            if (filterValue.length === 1) {
+                if (filter.includes(filterValue.toUpperCase())) {
+                    allFiltersIndex.push(filters.indexOf(filter))
+                }
+            } else {
+                if (filter.toLowerCase().includes(filterValue.toLowerCase())) {
+                    allFiltersIndex.push(filters.indexOf(filter))
+                }
             }
         }
     }
@@ -60,13 +60,16 @@ function multipleFilter(link, tableName, filterName, filterValues) {
 }
 
 chrome.action.onClicked.addListener((tab) => {
-    if (tab.url.includes("chrome://")) {
+    if (!tab.url.includes("https://mspl-1-c9455971.deta.app")) {
         chrome.tabs.create({url: "https://mspl-1-c9455971.deta.app"});
         chrome.tts.speak("Click on the extension icon to get started");
+        return;
     }
-    chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        files: ["content.js"],
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        const currentTab = tabs[0];
+        chrome.scripting.executeScript({
+            target: {tabId: currentTab.id}, files: ["content.js"],
+        })
     });
 });
 
@@ -81,10 +84,16 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         let tableName = 'RM_x0020_Price'
         let filterName = 'Plant'
         let newLink = addFilter(link, tableName, filterName, giveFilters(filterValues));
-        console.log(newLink);
         if (newLink) {
-            chrome.tabs.create({url: newLink});
-            link = `Opening ${request.text.toLowerCase().split("filter")[0].split("with")[0].trim()} with filter ${filter}`;
+            chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+                const currentTab = tabs[0];
+                if (currentTab.url.includes("https://mspl-1-c9455971.deta.app")) {
+                    chrome.tabs.create({url: newLink});
+                } else {
+                    chrome.tabs.update(currentTab.id, {url: newLink});
+                }
+            });
+            link = `Opening ${request.text.toLowerCase().split("filter")[0].split("with")[0].trim()} with filter ${filterValues.join(" ")}`;
         } else {
             link = "Sorry, I didn't get that";
         }
